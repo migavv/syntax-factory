03670bf6c360771460e79540f6aab08f926830ff
Garzon10 <garzonesteban3@gmail.com>
2024-03-25 15:45:45 -0500

    LE game
    Es u juegoi

---
+++ SyntaxFactory/Assets/Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+public class Movement : MonoBehaviour
+{
+   
+   public AIPath aiPath; 
+    void Start()
+    {
+        
+    }
+
+    void Update(){
+
+        
+
+    }
+
+}
