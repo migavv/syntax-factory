524d714eeaca479b5722cea2aea884d3306b7573
migavv <migaviria@outlook.com>
2024-07-21 22:22:59 -0500

    ahora si funciona

--- SyntaxFactory/Assets/BlocksEngine2/Scripts/EngineCore/Instruction/BlockInstructions/Custom/BE2_Cst_GoToDelivery.cs
+++ SyntaxFactory/Assets/BlocksEngine2/Scripts/EngineCore/Instruction/BlockInstructions/Custom/BE2_Cst_GoToDelivery.cs
@@ -9,6 +9,8 @@ using System.Threading;
 using UnityEngineInternal;
 using UnityEngine.XR;
 using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+using System;
+using Unity.Mathematics;
 
 // --- additional BE2 namespaces used for specific cases as accessing BE2 variables or the event manager
 // using MG_BlocksEngine2.Core;
@@ -28,27 +30,42 @@ public class BE2_Cst_GoToDelivery : BE2_InstructionBase, I_BE2_Instruction
       
     }
     public Transform targetObject; // Public field to assign the target object
-  
 
     // --- Method used to implement Function Blocks (will only be called by types: simple, condition, loop, trigger)
     public new void Function()
     {
-        targetObject = GameObject.Find("Destino").transform;
         //First
-        Agent.navMeshAgent.destination = targetObject.position;
-        Agent.navMeshAgent.isStopped = false;
-        float remainingDistance = Agent.navMeshAgent.remainingDistance;
-        Debug.Log("Remaining distance: " + remainingDistance + " / " + Agent.navMeshAgent.stoppingDistance);
-        Debug.Log("Location reached: " + (remainingDistance <= Agent.navMeshAgent.stoppingDistance));
-        bool isDestinationReached = !Agent.navMeshAgent.pathPending && remainingDistance <= Agent.navMeshAgent.stoppingDistance;
-        Debug.Log("Going to" + targetObject);
+        Vector2 pos = GameObject.Find("Robot").transform.position;
+        Vector2 des = targetObject.transform.position;
+        //Debug.Log("Delivery pos: " + des.x + " " + des.y); 
+        //Debug.Log("Robot: " + pos.x + " " + pos.y);
+        if (!done)
+        {
+            if (pos.x < des.x)
+            {
+                pos.x = Math.Min(pos.x + 0.1f, des.x);
+            }
+            else if (pos.x > des.x)
+            {
+                pos.x = Math.Max(pos.x - 0.1f, des.x);
+            }
+            if (pos.y < des.y)
+            {
+                pos.y = Math.Min(pos.y + 0.1f, des.y);
+            }
+            else if (pos.y > des.y)
+            {
+                pos.y = Math.Max(pos.y - 0.1f, des.y);
+            }
+
+            GameObject.Find("Robot").transform.position = new Vector2(pos.x, pos.y);
+        }
+
+        bool isDestinationReached = pos - des == new Vector2(0, 0);
+
         if (isDestinationReached)
         {
-            done = true;
-            Agent.navMeshAgent.isStopped = true;
-        }
-        if (done)
-        {
+            //Debug.Log("Destination delivery reached");
             ExecuteNextInstruction();
         }
     }
@@ -59,15 +76,15 @@ public class BE2_Cst_GoToDelivery : BE2_InstructionBase, I_BE2_Instruction
         Agent.navMeshAgent.isStopped = true;
     }
 
+    public override void OnStackActive()
+    {
+        ExecuteInUpdate = true;
+        targetObject = GameObject.Find("Destino").transform;
+        done = false;
+    }
+
     public new void Reset()
     {
         done = false;
-        Agent.navMeshAgent.isStopped = false;
-    }
-
-    public override void OnStackActive()
-    {
-        done = false;
-        Agent.navMeshAgent.isStopped = false;
     }
 }
--- SyntaxFactory/Assets/BlocksEngine2/Scripts/EngineCore/Instruction/BlockInstructions/Custom/BE2_Cst_GoToPickup.cs
+++ SyntaxFactory/Assets/BlocksEngine2/Scripts/EngineCore/Instruction/BlockInstructions/Custom/BE2_Cst_GoToPickup.cs
@@ -7,6 +7,8 @@ using MG_BlocksEngine2.Block.Instruction;
 using MG_BlocksEngine2.Block;
 using System.Threading;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using System;
 
 // --- additional BE2 namespaces used for specific cases as accessing BE2 variables or the event manager
 // using MG_BlocksEngine2.Core;
@@ -27,30 +29,44 @@ public class BE2_Cst_GoToPickup : BE2_InstructionBase, I_BE2_Instruction
       
     }
     public Transform targetObject; // Public field to assign the target object
-     
-        // Assign the target object via code
-        
-    
+
+    // Assign the target object via code
 
     // --- Method used to implement Function Blocks (will only be called by types: simple, condition, loop, trigger)
     public new void Function()
     {
-        targetObject = GameObject.Find("Destino2").transform;
         //First
-        Agent.navMeshAgent.destination = targetObject.position;
-        Agent.navMeshAgent.isStopped = false;
-        float remainingDistance = Agent.navMeshAgent.remainingDistance;
-        Debug.Log("Remaining distance: " + remainingDistance + " / " + Agent.navMeshAgent.stoppingDistance);
-        Debug.Log("Distance reached: " + remainingDistance + " / " + Agent.navMeshAgent.stoppingDistance);
-        bool isDestinationReached = !Agent.navMeshAgent.pathPending && remainingDistance <= Agent.navMeshAgent.stoppingDistance;
-        Debug.Log("Going to"+targetObject);
+        Vector2 pos = GameObject.Find("Robot").transform.position;
+        Vector2 des = targetObject.transform.position;
+        //Debug.Log("Pickup pos: " + des.x + " " + des.y);
+        //Debug.Log("Robot: " + pos.x + " " + pos.y);
+        if (!done)
+        {
+            if (pos.x < des.x)
+            {
+                pos.x = Math.Min(pos.x + 0.1f, des.x);
+            }
+            else if (pos.x > des.x)
+            {
+                pos.x = Math.Max(pos.x - 0.1f, des.x);
+            }
+            if (pos.y < des.y)
+            {
+                pos.y = Math.Min(pos.y + 0.1f, des.y);
+            }
+            else if (pos.y > des.y)
+            {
+                pos.y = Math.Max(pos.y - 0.1f, des.y);
+            }
+
+            GameObject.Find("Robot").transform.position = new Vector2(pos.x, pos.y);
+        }
+
+        bool isDestinationReached = pos - des == new Vector2(0, 0);
+
         if (isDestinationReached)
         {
-            Agent.navMeshAgent.isStopped = true;
-            done = true;
-        }
-        if (done)
-        {
+            //Debug.Log("Destination pickup reached");
             ExecuteNextInstruction();
         }
     }
@@ -63,13 +79,13 @@ public class BE2_Cst_GoToPickup : BE2_InstructionBase, I_BE2_Instruction
 
     public override void OnStackActive()
     {
+        ExecuteInUpdate = true;
+        targetObject = GameObject.Find("Destino2").transform;
         done = false;
-        Agent.navMeshAgent.isStopped = false;
     }
 
     public new void Reset()
     {
         done = false;
-        Agent.navMeshAgent.isStopped = false;
     }
 }
