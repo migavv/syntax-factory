b29071b96d14d3668c0a26c5207199352ecb9efe
Garzon10 <garzonesteban3@gmail.com>
2024-07-22 20:39:05 -0500

    Ajuset de speed

--- SyntaxFactory/Assets/BlocksEngine2/Scripts/EngineCore/Instruction/BlockInstructions/Custom/BE2_Cst_GoToDelivery.cs
+++ SyntaxFactory/Assets/BlocksEngine2/Scripts/EngineCore/Instruction/BlockInstructions/Custom/BE2_Cst_GoToDelivery.cs
@@ -43,19 +43,19 @@ public class BE2_Cst_GoToDelivery : BE2_InstructionBase, I_BE2_Instruction
         {
             if (pos.x < des.x)
             {
-                pos.x = Math.Min(pos.x + 0.1f, des.x);
+                pos.x = Math.Min(pos.x + 0.01f, des.x);
             }
             else if (pos.x > des.x)
             {
-                pos.x = Math.Max(pos.x - 0.1f, des.x);
+                pos.x = Math.Max(pos.x - 0.01f, des.x);
             }
             if (pos.y < des.y)
             {
-                pos.y = Math.Min(pos.y + 0.1f, des.y);
+                pos.y = Math.Min(pos.y + 0.01f, des.y);
             }
             else if (pos.y > des.y)
             {
-                pos.y = Math.Max(pos.y - 0.1f, des.y);
+                pos.y = Math.Max(pos.y - 0.01f, des.y);
             }
 
             GameObject.Find("Robot").transform.position = new Vector2(pos.x, pos.y);
